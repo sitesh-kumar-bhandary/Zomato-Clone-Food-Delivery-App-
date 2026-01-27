d4dadcae5690adb81b614798801232fc0a7b508c
Sitesh01 <siteshisunique1998@gmail.com>
2026-01-27 11:00:33 +0530

    2nd - pushed project basic structures and modifed gitignore file

---
+++ src/main/java/com/siteshkumar/zomato_clone_backend/ZomatoCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siteshkumar.zomato_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoCloneBackendApplication.class, args);
+	}
+
+}
