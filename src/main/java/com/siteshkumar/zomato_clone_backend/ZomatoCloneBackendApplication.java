0a0686ee2d181c09218b463c636c2139bd9f5966
Sitesh01 <siteshisunique1998@gmail.com>
2026-02-02 00:08:06 +0530

    4th - Implemented the swagger ui and .env files to protect the sensitive data

--- src/main/java/com/siteshkumar/zomato_clone_backend/ZomatoCloneBackendApplication.java
+++ src/main/java/com/siteshkumar/zomato_clone_backend/ZomatoCloneBackendApplication.java
@@ -8,6 +8,6 @@ public class ZomatoCloneBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZomatoCloneBackendApplication.class, args);
+		System.out.println("This project is developed by Sitesh Kumar");
 	}
-
 }
---
+++ src/main/java/com/siteshkumar/zomato_clone_backend/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.siteshkumar.zomato_clone_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI blogApi(){
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+            ))
+
+            .info(new Info()
+                .title("Zomato Clone Backend Application")
+                .description("This the complete backend for zomato clone")
+                .version("1.0.0")
+            );
+    }
+}
